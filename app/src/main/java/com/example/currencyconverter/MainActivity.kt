fb8ef0b3d5e52c0a667be9ad123f4a0917147f3a
lucas-delazeri <lucasdelazeris@gmail.com>
2026-08-03 15:37:30 -0300

    feat: validate empty input before converting currency

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -3,18 +3,34 @@ package com.example.currencyconverter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener {
+
+            val text = binding.editConverter.text.toString()
+
+            if (text.isBlank()) {
+                binding.editConverter.error = "Digite um valor"
+                return@setOnClickListener
+            }
+
+            val euro = text.toDouble()
+            val dollars = String.format("%.2f", euro * 0.8)
+
+            binding.textViewCurrencyResult.text = dollars
         }
+
     }
 }
